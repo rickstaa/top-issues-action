b0f81708fcdc221589fc0c8191dcce6fa7e825b5
rickstaa <rick.staa@outlook.com>
2022-07-21 12:33:51 +0200

    feat: log top 10 issues
    This commit logs the top 10 issues to the console.

---
+++ src/helpers.ts
@@ -0,0 +1,3 @@
+/**
+ * @file Contains action helper functions.
+ */
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,106 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import dotenv from 'dotenv'
+
+import {getOctokit} from '@actions/github'
+
+dotenv.config()
+
+// Types
+interface IssueNode {
+  number: number
+  title: string
+  positive: {totalCount: number}
+  negative: {totalCount: number}
+  labels: {nodes: {name: string}[]}
+}
+interface OpenIssues {
+  nodes: IssueNode[]
+  pageInfo: {endCursor: string; hasNextPage: boolean}
+}
+interface IssuesResponse {
+  repository: {
+    open_issues: OpenIssues
+  }
+}
+
+// Create octokit client
+const octokit = getOctokit(`${process.env.GITHUB_TOKEN}`)
+
+const fetchOpenIssues = async (
+  user: string,
+  repo: string
+): Promise<IssueNode[]> => {
+  const {repository} = await octokit.graphql<IssuesResponse>(
+    `
+      {
+        repository(owner: "${user}", name: "${repo}") {
+          open_issues: issues(first: 100, states: OPEN) {
+            nodes {
+              number
+              title
+              positive: reactions(content: THUMBS_UP) {
+                totalCount
+              }
+              negative: reactions(content: THUMBS_DOWN) {
+                totalCount
+              }
+              labels(first: 10) {
+                nodes {
+                  name
+                }
+              }
+            }
+            pageInfo {
+              endCursor
+              hasNextPage
+            }
+          }
+        }
+      }
+    `
+  )
+  return repository.open_issues.nodes
+}
+
+const getTopIssues = (issues: IssueNode[], size: number): IssueNode[] => {
+  issues = issues.sort((a: IssueNode, b: IssueNode) => {
+    return (
+      b.positive.totalCount -
+      b.negative.totalCount -
+      (a.positive.totalCount - a.negative.totalCount)
+    )
+  })
+  return issues.slice(0, size)
+}
 
 async function run(): Promise<void> {
+  const user = 'anuraghazra'
+  const repo = 'github-readme-stats'
+
+  // Retrieve issues
+  let issues: IssueNode[]
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    issues = await fetchOpenIssues(user, repo)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    // TODO: see https://github.com/octokit/request-error.js/issues/246
+    if (error instanceof Error && error.name === 'HttpError') {
+      console.error(`GraphQl request failed due to: ${error.message}`)
+    }
+    throw error
   }
+
+  // Retrieve top 10 issues
+  const topIssues = getTopIssues(issues, 10)
+
+  // Print Top Issues
+  console.log('Top 10 issues:')
+  for (const issue of topIssues) {
+    console.log(
+      `  - ${issue.title}: ${
+        issue.positive.totalCount - issue.negative.totalCount
+      }`
+    )
+  }
+  console.log('title')
 }
 
 run()
